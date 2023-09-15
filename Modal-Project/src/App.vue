608339579dc2574a66ae61ac427e8c276afcd04e
adamaconguli <waltson2003@gmail.com>
2023-09-15 16:31:27 +0800

    lesson 5: click event modifier

---
+++ Modal-Project/src/App.vue
@@ -0,0 +1,38 @@
+<script>
+  import Modal from './components/Modal.vue'
+
+  export default {
+    name: 'App',
+    components: { Modal },
+    data() {
+      return {
+        title: 'My First Vue App :)',
+        header: 'Sign up for the Giveaway',
+        text: 'Grab your ninja swag for half price!',
+        showModal: false,
+      }
+    },
+    methods: {
+      toggleModal() {
+        this.showModal = !this.showModal
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  </div>
+  <button @click.alt="toggleModal">open modal (alt)</button>
+</template>
+
+<style scoped>
+  h1 {
+    border-bottom: 1px solid #ddd;
+    display: inline-block;
+    padding-bottom: 10px;
+  }
+</style>
---
+++ Modal-Project/src/components/Modal.vue
@@ -0,0 +1,40 @@
+<script>
+  export default {
+    props: ['header', 'text', 'theme'],
+    methods: {
+      closeModal() {
+        this.$emit('close')
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+  }
+  .backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+  }
+  .sale {
+    background: crimson;
+    color: white;
+  }
+</style>
