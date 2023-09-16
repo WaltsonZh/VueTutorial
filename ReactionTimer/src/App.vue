32ee57758d41933273f6fa89ee42a582b74df9d0
adamaconguli <waltson2003@gmail.com>
2023-09-16 17:15:42 +0800

    lesson 6: custom event with data

--- ReactionTimer/src/App.vue
+++ ReactionTimer/src/App.vue
@@ -8,12 +8,20 @@
       return {
         isPlaying: false,
         delay: null,
+        score: null,
+        showResult: false,
       }
     },
     methods: {
       start() {
         this.delay = 2000 + Math.random() * 5000
         this.isPlaying = true
+        this.showResult = false
+      },
+      endGame(reactionTime) {
+        this.score = reactionTime
+        this.isPlaying = false
+        this.showResult = true
       },
     },
   }
@@ -22,12 +30,13 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">play</button>
-  <Block :delay="delay" v-if="isPlaying" />
+  <Block :delay="delay" v-if="isPlaying" @end="endGame" />
+  <p v-if="showResult">Reaction time: {{ score }} ms</p>
 </template>
 
 <style scoped>
   h1 {
-    margin: 2em;
+    margin-top: 2em;
   }
   button {
     width: 200px;
--- ReactionTimer/src/components/Block.vue
+++ ReactionTimer/src/components/Block.vue
@@ -22,7 +22,7 @@
       },
       stopTimer() {
         clearInterval(this.timer)
-        console.log(this.reactionTime)
+        this.$emit('end', this.reactionTime)
       }
     }
   }
