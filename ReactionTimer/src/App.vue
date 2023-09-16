edea8fa6ace297bb6223e189728428d93d8b2deb
adamaconguli <waltson2003@gmail.com>
2023-09-16 17:02:03 +0800

    lesson 6: life cycle hooks

--- ReactionTimer/src/App.vue
+++ ReactionTimer/src/App.vue
@@ -1,11 +1,39 @@
 <script>
-  
+  import Block from './components/Block.vue'
+
+  export default {
+    name: 'App',
+    components: { Block },
+    data() {
+      return {
+        isPlaying: false,
+        delay: null,
+      }
+    },
+    methods: {
+      start() {
+        this.delay = 2000 + Math.random() * 5000
+        this.isPlaying = true
+      },
+    },
+  }
 </script>
 
 <template>
   <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block :delay="delay" v-if="isPlaying" />
 </template>
 
 <style scoped>
-  
+  h1 {
+    margin: 2em;
+  }
+  button {
+    width: 200px;
+    height: 2em;
+    border-radius: 10px;
+    background: #81fadc;
+    cursor: pointer;
+  }
 </style>
---
+++ ReactionTimer/src/components/Block.vue
@@ -0,0 +1,39 @@
+<script>
+  export default {
+    props: ['delay'],
+    data() {
+      return {
+        showBlock: false,
+      }
+    },
+    mounted() {
+      console.log('Block mounted')
+      setTimeout(() => {
+        this.showBlock = true
+        console.log(this.delay)
+      }, this.delay)
+    },
+    updated() {
+      console.log('Block updated')
+    },
+    unmounted() {
+      console.log('Block unmounted')
+    }
+  }
+</script>
+
+<template>
+  <div class="block" v-if="showBlock">click me</div>
+</template>
+
+<style scoped>
+  .block {
+    width: 400px;
+    border-radius: 20px;
+    background: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+  }
+</style>
