b4ce9ac233fdced1cc4e1d9628fda6b3d9018c70
adamaconguli <waltson2003@gmail.com>
2023-09-18 20:53:30 +0800

    lesson 8: folder structure

--- NinjaJobs/src/App.vue
+++ NinjaJobs/src/App.vue
@@ -2,6 +2,7 @@
   <nav>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/jobs">Jobs</RouterLink>
   </nav>
 
   <RouterView />
@@ -12,5 +13,21 @@
 </script>
 
 <style scoped>
-  
+  nav {
+    padding: 1em;
+    display: flex;
+    gap: 1em;
+  }
+  nav a {
+    font-size: 1.2em;
+    text-decoration: none;
+    color: black;
+    font-weight: 700;
+    padding: 0.5em;
+    border-radius: 10px;
+  }
+  nav a.router-link-active {
+    background: crimson;
+    color: white;
+  }
 </style>
