86d19c51899e4713b9d8a0e1fa5825f758ea64c2
adamaconguli <waltson2003@gmail.com>
2023-09-19 20:05:05 +0800

    lesson 10: template refs

--- DojoBlog/src/views/HomeView.vue
+++ DojoBlog/src/views/HomeView.vue
@@ -1,22 +1,38 @@
 <template>
   <div>
     Home
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
   export default {
     setup() {
       let name = 'mario'
       let age = 30
 
+      const p = ref('hello')
+      console.log(p, p.value)
+
       const handleClick = () => {
-        console.log('clicked')
+        console.log(p, p.value)
+        p.value.classList.add('test')
+        p.value.textContent = 'hello, ninjas'
       }
 
-      return { name, age, handleClick }
+      return { name, age, handleClick, p }
     },
   }
 </script>
+
+<style scoped>
+  div {
+    margin: 2em;
+    display: grid;
+    place-items: center;
+    gap: 1em;
+  }
+</style>
