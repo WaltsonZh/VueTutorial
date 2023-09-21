645d24359f849760481e9680543203ce7e5e30bc
adamaconguli <waltson2003@gmail.com>
2023-09-21 17:05:06 +0800

    lesson 10: watch and watchEffect

--- DojoBlog/src/views/HomeView.vue
+++ DojoBlog/src/views/HomeView.vue
@@ -4,14 +4,28 @@
     <input type="text" v-model="search" />
     <p>search term - {{ search }}</p>
     <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    <button @click="handleClick">stop watching</button>
   </div>
 </template>
 
 <script setup>
-  import { ref, computed } from 'vue'
+  import { ref, computed, watch, watchEffect } from 'vue'
 
   const search = ref('')
   const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
+  
+  const stopWatch = watch(search, () => {
+    console.log('watch function run')
+  })
+
+  const stopEffect = watchEffect(() => {
+    console.log('watchEffect function ran')
+  })
+
+  const handleClick = () => {
+    stopWatch()
+    stopEffect()
+  }
 
   const matchingNames = computed(() => {
     return names.value.filter((name) => name.includes(search.value))
