18036bac99f69b91ba1109712228f45c1a709b70
adamaconguli <waltson2003@gmail.com>
2023-09-21 16:36:28 +0800

    lesson 10: Refs and Reactive

--- DojoBlog/src/views/HomeView.vue
+++ DojoBlog/src/views/HomeView.vue
@@ -1,31 +1,27 @@
 <template>
-  <div>
-    Home
-    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">click me</button>
-    <button @click="age++">add 1 to age</button>
-    <input type="text" v-model="name">
+  <div class="home">
+    <h1>Home</h1>
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Update ninja one</button>
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">Update ninja two</button>
   </div>
 </template>
 
-<script>
-import { ref } from 'vue'
+<script setup>
+  import { ref, reactive } from 'vue'
 
-  export default {
-    setup() {
-      const name = ref('mario')
-      const age = ref(30)
+  const ninjaOne = ref({ name: 'mario', age: 30})
+  const ninjaTwo = reactive({ name: 'luigi', age: 35 })
 
-      const p = ref('hello')
-      console.log(p, p.value)
+  const updateNinjaOne = () => {
+    ninjaOne.value.age = 40
+  }
 
-      const handleClick = () => {
-        name.value = 'luigi'
-        age.value = 35
-      }
-
-      return { name, age, handleClick, p }
-    },
+  const updateNinjaTwo = () => {
+    ninjaTwo.age = 45
   }
 </script>
 
