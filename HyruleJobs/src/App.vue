61e420bab764f6feb74ac31c9f1020d18f8e9b42
adamaconguli <waltson2003@gmail.com>
2023-09-23 21:57:40 +0800

    lesson 2, 3: options & composition API components typescript

--- HyruleJobs/src/App.vue
+++ HyruleJobs/src/App.vue
@@ -1,9 +1,51 @@
 <template>
   <div class="app">
-    hello, hyrule
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('zelda')">change name</button>
+    <button @click="changeAge('35')">change age</button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<!--options API-->
+<!-- <script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'App',
+    components: {},
+    data() {
+      return {
+        name: 'Link',
+        age: 25 as number | string,
+      }
+    },
+    methods: {
+      changeName(name: string) {
+        this.name = name
+        return name
+      },
+      changeAge(age: string | number) {
+        this.age = age
+        return age
+      },
+    },
+  })
+</script> -->
+
+<!--composition API-->
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const name = ref('Link')
+  const age = ref<number | string>(25)
+
+  const changeName = (newName: string) => {
+    name.value = newName
+  }
+
+  const changeAge = (newAge: number | string) => {
+    age.value = newAge
+  }
+</script>
 
 <style scoped></style>
