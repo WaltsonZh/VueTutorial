c4ff2755ee0879036f6042c7fcc231282f249b28
adamaconguli <waltson2003@gmail.com>
2023-09-23 22:09:49 +0800

    setup app

--- HyruleJobs/src/App.vue
+++ HyruleJobs/src/App.vue
@@ -1,51 +1,20 @@
 <template>
   <div class="app">
-    <p>{{ name }} - {{ age }}</p>
-    <button @click="changeName('zelda')">change name</button>
-    <button @click="changeAge('35')">change age</button>
+    {{ JSON.stringify(jobs) }}
   </div>
 </template>
 
-<!--options API-->
-<!-- <script lang="ts">
-  import { defineComponent } from 'vue'
-
-  export default defineComponent({
-    name: 'App',
-    components: {},
-    data() {
-      return {
-        name: 'Link',
-        age: 25 as number | string,
-      }
-    },
-    methods: {
-      changeName(name: string) {
-        this.name = name
-        return name
-      },
-      changeAge(age: string | number) {
-        this.age = age
-        return age
-      },
-    },
-  })
-</script> -->
-
-<!--composition API-->
 <script setup lang="ts">
   import { ref } from 'vue'
+  import Job from './types/Jobs.ts'
 
-  const name = ref('Link')
-  const age = ref<number | string>(25)
-
-  const changeName = (newName: string) => {
-    name.value = newName
-  }
-
-  const changeAge = (newAge: number | string) => {
-    age.value = newAge
-  }
+  const jobs = ref<Job[]>([
+    { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+    { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+    { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+    { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+    { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' },
+  ])
 </script>
 
 <style scoped></style>
